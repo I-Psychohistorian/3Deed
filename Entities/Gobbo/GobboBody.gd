2d6cab5f74eb40c5bd82833369eebdf10910e2f7
I-Psychohistorian <92660661+I-Psychohistorian@users.noreply.github.com>
2023-01-01 12:47:39 -0800

    Head Turning!
    Gobbo look works, added dopey animation for test.

--- Entities/Gobbo/GobboBody.gd
+++ Entities/Gobbo/GobboBody.gd
@@ -8,9 +8,14 @@ extends KinematicBody
 onready var sight_area = $Sight
 onready var head_joint = $GobboHead
 
+var anim_states = ["Idle", "Animate"]
+var current_state = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$GobboHead/FaceAnim.play("Blink")
+	current_state = anim_states[0]
+	print(current_state)
 	pass # Replace with function body.
 
 
@@ -23,7 +28,16 @@ func test_look():
 	var bodies = sight_area.get_overlapping_bodies()
 	for body in bodies:
 		if body.is_in_group("Player"):
-			var look_point = body.global_transform.origin + Vector3(0, 1, 0)
+			var look_point = body.global_transform.origin + Vector3(0, 0.7, 0)
 			head_joint.look_at(look_point, Vector3.UP)
-			head_joint.rotation.z = clamp(self.rotation.z, deg2rad(-50), deg2rad(80))
-			#clamping broken, will need to fix
+			if head_joint.rotation.y > deg2rad(70):
+				head_joint.rotation.y = deg2rad(70)
+			if head_joint.rotation.y < deg2rad(-70):
+				head_joint.rotation.y = deg2rad(-70)
+			if head_joint.rotation.x > deg2rad(45):
+				head_joint.rotation.x = deg2rad(45)
+			if head_joint.rotation.x < deg2rad(-45):
+				head_joint.rotation.x = deg2rad(-45)
+
+func _on_DebugReport_timeout():
+	$BodyAnim.play("Dopey")
